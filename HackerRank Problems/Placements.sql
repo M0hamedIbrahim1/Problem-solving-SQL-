7f3ef939d27981fd2b031cf0bbbf4148dd528ecd
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-05 22:27:42 +0200

    Create Placements.sql

---
+++ HackerRank Problems/Placements.sql
@@ -0,0 +1,10 @@
+/* https://www.hackerrank.com/challenges/placements/problem */
+
+Select S.Name
+From ( Students S join Friends F Using(ID)
+       join Packages P1 on S.ID=P1.ID
+       join Packages P2 on F.Friend_ID=P2.ID)
+Where P2.Salary > P1.Salary
+Order By P2.Salary;
+
+
