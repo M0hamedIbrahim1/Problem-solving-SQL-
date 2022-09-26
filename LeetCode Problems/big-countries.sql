b942a4f359d6d2bc5f0397281ce9d40e3ddcf2c4
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-09-26 22:57:18 +0200

    Create big-countries.sql

---
+++ LeetCode Problems/big-countries.sql
@@ -0,0 +1,7 @@
+/*
+    link   :  https://leetcode.com/problems/big-countries/
+    author : Mohamed Ibrahim
+*/
+
+select name,population,area from World
+where area >= 3000000 or population >= 25000000;
