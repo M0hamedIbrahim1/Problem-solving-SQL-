3ce99e487207a59ff6a4cf3046ec7ed5f04d5183
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-09-30 01:40:56 +0200

    Update 1527. Patients With a Condition.sql

--- LeetCode Problems/1527. Patients With a Condition.sql
+++ LeetCode Problems/1527. Patients With a Condition.sql
@@ -1,8 +1,9 @@
 /*
     Link   : https://leetcode.com/problems/patients-with-a-condition/?envType=study-plan&id=sql-i
     author : Mohamed Ibrahim
+
 */
 
-select patient_id , patient_name , conditions from Patients 
-where conditions like '%DIAB1%'
+select patient_id, patient_name, conditions from patients
+where conditions like 'DIAB10%' or conditions like '% DIAB10%'; 
 
