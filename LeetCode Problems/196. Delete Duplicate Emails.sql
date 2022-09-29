c75565274da6481980997a0efc1966cabe1c71d7
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-09-29 20:13:11 +0200

    Create 196. Delete Duplicate Emails.sql

---
+++ LeetCode Problems/196. Delete Duplicate Emails.sql
@@ -0,0 +1,9 @@
+/*
+      link   : https://leetcode.com/problems/delete-duplicate-emails/
+      author : Mohamed Ibrahim
+*/
+
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email
+AND p1.Id > p2.Id
