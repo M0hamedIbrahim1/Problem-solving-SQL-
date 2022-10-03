c20ffe0ec1183a310d211a52f63c3ea578a2af25
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-10-03 20:12:20 +0200

    Create 1693. Daily Leads and Partners.sql

---
+++ LeetCode Problems/1693. Daily Leads and Partners.sql
@@ -0,0 +1,10 @@
+/*
+    link   : https://leetcode.com/problems/daily-leads-and-partners/
+    author : Mohamed Ibrahim
+*/
+
+
+select date_id , make_name , count(distinct lead_id ) as unique_leads  , count(distinct partner_id ) as unique_partners 
+from DailySales 
+group by date_id , make_name 
+
