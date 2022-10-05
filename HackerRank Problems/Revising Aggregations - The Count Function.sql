f4082f7d39a55f70aa265451002bf1f3a9faf345
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-10-05 23:22:28 +0200

    Create Revising Aggregations - The Count Function.sql

---
+++ HackerRank Problems/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,12 @@
+/*
+
+  Link   : https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+  author : Mohamed Ibrahim
+
+*/
+
+
+select count(NAME) FROM CITY where POPULATION > 100000;
+
+
+
