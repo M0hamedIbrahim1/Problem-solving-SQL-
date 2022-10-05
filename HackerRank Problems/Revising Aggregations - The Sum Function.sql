b46d2b290c71f70d5888aca521577e14473d1499
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-10-05 23:23:45 +0200

    Create Revising Aggregations - The Sum Function.sql

---
+++ HackerRank Problems/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,11 @@
+/*
+
+  Link   : https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+  author : Mohamed Ibrahim
+
+*/
+
+
+select sum(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+
+
