649d20c455dc19619e1413bad23a8ebaf3303db0
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-10-05 23:25:31 +0200

    Create Revising Aggregations - Averages.sql

---
+++ HackerRank Problems/Revising Aggregations - Averages.sql
@@ -0,0 +1,10 @@
+/*
+
+  Link   : https://www.hackerrank.com/challenges/revising-aggregations-sum/problem?isFullScreen=true
+  author : Mohamed Ibrahim
+
+*/
+
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+
