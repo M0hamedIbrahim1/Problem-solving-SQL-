7503b8c451716e0a58f36a6bb6e8fea710b50e1f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-11-06 19:24:31 +0200

    Create User Shopping Sprees.sql

---
+++ DataLemur Problems/User Shopping Sprees.sql
@@ -0,0 +1,38 @@
+/*
+
+  Link   : https://datalemur.com/questions/amazon-shopping-spree
+  author : Mohamed Ibrahim
+
+
+*/
+
+
+SELECT Customer_id , SUM(Revenue) FROM 
+(
+SELECT customer_id AS Customer_id,
+       product,CASE WHEN product = 'Photoshop' THEN 0 ELSE revenue END AS Revenue
+FROM adobe_transactions 
+
+) AS G1
+WHERE Customer_id in (
+    SELECT DISTINCT customer_id FROM adobe_transactions 
+    WHERE product = 'Photoshop'
+)
+GROUP BY Customer_id
+
+
+other sol : 
+
+
+SELECT 
+  customer_id,
+  SUM(revenue) AS revenue
+FROM adobe_transactions
+WHERE customer_id IN (
+  SELECT customer_id FROM adobe_transactions WHERE product = 'Photoshop')
+  AND product <> 'Photoshop'
+GROUP BY customer_id
+ORDER BY customer_id;
+
+
+
